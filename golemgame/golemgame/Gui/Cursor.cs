20a153137e48732cb174311bb3b894a0541e75a8
Campbell Brown <campbellbrown093@gmail.com>
2021-02-25 20:32:59 +1300

    Added cursor functionality

---
+++ golemgame/golemgame/Gui/Cursor.cs
@@ -0,0 +1,33 @@
+﻿using golemgame.Models;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace golemgame.Gui
+{
+    public class Cursor
+    {
+        private Sprite _sprite;
+        private Vector2 _position;
+
+        public Cursor()
+        {
+            _position = Vector2.Zero;
+            _sprite = new Sprite(Game1.textures["cursor"]);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            _position = Game1.mousePosition;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            _sprite.Draw(spriteBatch, _position);
+        }
+    }
+}
---
+++ golemgame/golemgame/Managers/CursorManager.cs
@@ -0,0 +1,31 @@
+﻿using golemgame.Gui;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace golemgame.Managers
+{
+    public class CursorManager
+    {
+        private Cursor _cursor;
+
+        public CursorManager()
+        {
+            _cursor = new Cursor();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            _cursor.Update(gameTime);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            _cursor.Draw(spriteBatch);
+        }
+    }
+}
