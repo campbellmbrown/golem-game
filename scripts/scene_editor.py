ed84258e0b233572951d494ffbc0575799176091
Campbell Brown <campbellbrown093@gmail.com>
2021-03-17 19:35:30 +1300

    Massive restructure of level editor

--- scripts/scene_editor.py
+++ scripts/scene_editor.py
@@ -6,6 +6,7 @@ import os
 import io
 
 SPRITE_SHEET_PATH = os.getcwd() + "/tilesheets"
+RESOURCES_PATH = os.getcwd() + "/scene_editor_textures"
 TILE_SIZE = 16  # Pixels
 DISPLAY_TILES_X = 16
 DISPLAY_TILES_Y = 10
@@ -13,47 +14,107 @@ TILES_X = 32
 TILES_Y = 20
 
 
-class Tile:
+class MapTile:
     def __init__(self, x, y):
         self.x = x
         self.y = y
         self.texture_rectangle = (0, 0, TILE_SIZE, TILE_SIZE)
+        self.data = 0
+
+class AvaliableTile:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.texture_rectangle = (x * TILE_SIZE, y * TILE_SIZE, (x + 1) * TILE_SIZE, TILE_SIZE)
+        self.data = 0
+
+class Spritesheet:
+    def __init__(self, path):
+        self.path = path
+        self.width, self.height = self.find_size(path)
+
+    def find_size(self, path):
+        img = Image.open(path)
+        width, height = img.size
+        return [int(width / TILE_SIZE), int(height / TILE_SIZE)]
 
 class SceneEditorGUI:
     def __init__(self):
         # Setting up tile grid
-        self.tile_grid = [[Tile(i, j) for i in range(TILES_X)] for j in range(TILES_Y)]
+        self.tile_grid = [[MapTile(i, j) for i in range(TILES_X)] for j in range(TILES_Y)]
+
+        blank_path = os.path.join(RESOURCES_PATH, "blank_tile.png")
+        spritesheet_path = os.path.join(SPRITE_SHEET_PATH, "test_tilesheet.png")
+        self.spritesheet = Spritesheet(spritesheet_path)
+        self.avaliable_tile_grid = [[AvaliableTile(i, j) for i in range(self.spritesheet.width)] for j in range(self.spritesheet.height)]
+
         # Setting up user interface
         layout = self.create_layout()
-        self.window = sg.Window(title="Scene Editor", layout=layout)
+        self.window = sg.Window(title="Scene Editor", layout=layout, finalize=True)
+        
+        # Defaulting map grid to blank tile
+        for j in range(DISPLAY_TILES_Y):
+            for i in range(DISPLAY_TILES_X):
+                key = self.get_key_from_grid("MAP", i, j)
+                self.window[key].update(data=self.convert_image_to_bytes(self.tile_grid[j][i], blank_path))
+
+        # Filling in the avaliable tile grid
+        for j in range(self.spritesheet.height):
+            for i in range(self.spritesheet.width):
+                key = self.get_key_from_grid("AVALIABLE", i, j)
+                tile = self.avaliable_tile_grid[j][i]
+                # Reading the image and extracting bytes data
+                self.convert_image_to_bytes(tile, self.spritesheet.path)
+                tile_data = tile.data
+                # Updating the image with the tile image
+                self.window[key].update(data=tile_data)
+
+        self.window["-CURRENT_TILE-"].update(data=self.avaliable_tile_grid[0][0].data)
+
+    def get_image_element(self, key, i_idx, j_idx):
+        return self.window[(self.get_key_from_grid(key, i_idx, j_idx))]
+
+    def get_key_from_grid(self, key, i_idx, j_idx):
+        return "-" + key + "_" + str(i_idx) + "_" + str(j_idx) + "_"
 
     def create_layout(self):
-        # First column
-        file_list_column = [
-            [sg.Button("Refresh", key="-REFRESH-")],
-            [sg.Text("Select the sprite sheet:")],
-            [sg.Listbox(values=[], enable_events=True, size=(65, 5), key="-FILE LIST-")],
-            [sg.Button("Exit")],
+        # Creating the avaliable tiles section
+        avaliable_column = [
+             [sg.Text("Avaliable tiles:")],
         ]
-        # Second column
-        image_viewer_column = [[sg.Image(key="-IMAGE_" + str(i) + "_" + str(j) + "_", pad=(0,0)) for i in range(DISPLAY_TILES_X)] for j in range(DISPLAY_TILES_Y)]
-        # Bringing together columns
+        for j in range(self.spritesheet.height):
+            avaliable_column.append([sg.Image(key=self.get_key_from_grid("AVALIABLE", i, j), pad=(0,0), enable_events=True) for i in range(self.spritesheet.width)])
+
+        # Creating the map section
+        map_column = [
+            [sg.Text("Map:")],
+        ]
+        for j in range(DISPLAY_TILES_Y):
+            map_column.append([sg.Image(key=self.get_key_from_grid("MAP", i, j), pad=(0,0), enable_events=True) for i in range(DISPLAY_TILES_X)])
+
+        # Creating the information section
+        information_column = [
+            [sg.Text("Selected tile:")],
+            [sg.Image(key="-CURRENT_TILE-")],
+            [sg.Text("Avaliable tiles:")],
+        ]        
+        
+        # Bringing together section
         layout = [
-            [
-                sg.Column(file_list_column),
-                sg.VSeperator(),
-                sg.Column(image_viewer_column)
-            ]
+            [sg.Column(information_column)],
+            [sg.Column(avaliable_column), sg.VSeperator(), sg.Column(map_column)],
+            [sg.Button("Exit")]
         ]
+
         return layout
 
-    def convert_to_bytes(self, tile, file_path):
+    def convert_image_to_bytes(self, tile, file_path):
         img = Image.open(file_path)
-        # cur_width, cur_height = img.size
-        img = img.crop(box=(tile.texture_rectangle[0], tile.texture_rectangle[1], tile.texture_rectangle[2], tile.texture_rectangle[3]))    
+        img = img.crop(box=(tile.texture_rectangle[0], tile.texture_rectangle[1], tile.texture_rectangle[2], tile.texture_rectangle[3]))
         bio = io.BytesIO()
         img.save(bio, format="PNG")
         del img
+        tile.data = bio.getvalue()
         return bio.getvalue()
 
     def loop(self):
@@ -61,13 +122,20 @@ class SceneEditorGUI:
             event, values = self.window.read()
             if event == "Exit" or event == sg.WIN_CLOSED:
                 break
+            '''
             if event == "-REFRESH-":
                 self.update_filelist(SPRITE_SHEET_PATH)
             elif event == "-FILE LIST-":  # A file was chosen from the listbox
-                filename = os.path.join(SPRITE_SHEET_PATH, values["-FILE LIST-"][0])
-                for j in range(DISPLAY_TILES_Y):
-                    for i in range(DISPLAY_TILES_X):
-                        self.window["-IMAGE_" + str(i) + "_" + str(j) + "_"].update(data=self.convert_to_bytes(self.tile_grid[j][i], filename))
+                # filename = os.path.join(SPRITE_SHEET_PATH, values["-FILE LIST-"][0])
+                # for j in range(DISPLAY_TILES_Y):
+                #     for i in range(DISPLAY_TILES_X):
+                #         self.window["-IMAGE_" + str(i) + "_" + str(j) + "_"].update(data=self.convert_image_to_bytes(self.tile_grid[j][i], filename))
+            for j in range(DISPLAY_TILES_Y):
+                for i in range(DISPLAY_TILES_X):
+                    if event == ("-IMAGE_" + str(i) + "_" + str(j) + "_"):
+                        print("{}, {}".format(i, j))
+            '''
+            continue
         self.window.close()
 
     def update_filelist(self, folder):
