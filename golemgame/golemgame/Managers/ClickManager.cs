113a014de2f283e470ab156a23df52b28bdc76ee
Campbell Brown <campbellbrown093@gmail.com>
2021-07-01 20:05:12 +1200

    Added screen buttons

---
+++ golemgame/golemgame/Managers/ClickManager.cs
@@ -0,0 +1,91 @@
+﻿using golemgame.Models;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace golemgame.Managers
+{
+    public class ClickManager
+    {
+        public delegate void MethodDelegate();
+
+        private readonly DebugManager _debugManager;
+        private readonly ViewManager _viewManager;
+        private bool _leftClickHeldDown;
+        private bool _rightClickHeldDown;
+
+        private List<ScreenLevelLeftClick> _screenLevelLeftClicks;
+
+        public class ScreenLevelLeftClick
+        {
+            public ScreenButton button { get; set; }
+            public MethodDelegate outputFunc { get; set; }
+
+            public ScreenLevelLeftClick(ScreenButton button, MethodDelegate outputFunc)
+            {
+                this.button = button;
+                this.outputFunc = outputFunc;
+            }
+        }
+
+        public ClickManager(DebugManager debugManager, ViewManager viewManager)
+        {
+            _debugManager = debugManager;
+            _viewManager = viewManager;
+            _leftClickHeldDown = false;
+            _rightClickHeldDown = false;
+
+            _screenLevelLeftClicks = new List<ScreenLevelLeftClick>();
+        }
+
+        public void Update()
+        {
+            MouseState mouseState = Mouse.GetState();
+            // Left click
+            if (mouseState.LeftButton == ButtonState.Pressed)
+            {
+                if (!_leftClickHeldDown)
+                {
+                    LeftClick();
+                }
+                _leftClickHeldDown = true;
+            }
+            else _leftClickHeldDown = false;
+
+            // Right click
+            if (mouseState.RightButton == ButtonState.Pressed)
+            {
+                if (!_rightClickHeldDown)
+                {
+                    RightClick();
+                }
+                _rightClickHeldDown = true;
+            }
+            else _rightClickHeldDown = false;
+        }
+
+        public void LeftClick()
+        {
+            _debugManager.AddDebugMessage("Left click", DebugLevel.Debug);
+            foreach (var screenLevelLeftClick in _screenLevelLeftClicks)
+            {
+                if (screenLevelLeftClick.button.clickRectangle.Contains(_viewManager.mousePosition))
+                {
+                    screenLevelLeftClick.outputFunc();
+                }
+            }
+        }
+
+        public void RightClick()
+        {
+            _debugManager.AddDebugMessage("Right click", DebugLevel.Debug);
+        }
+
+        public void AddScreenLevelLeftClick(ScreenButton button, MethodDelegate func)
+        {
+            _screenLevelLeftClicks.Add(new ScreenLevelLeftClick(button, func));
+        }
+    }
+}
---
+++ golemgame/golemgame/Models/ScreenButton.cs
@@ -0,0 +1,43 @@
+﻿using golemgame.Managers;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace golemgame.Models
+{
+    public class ScreenButton
+    {
+        private readonly DebugManager _debugManager;
+        private readonly ViewManager _viewManager;
+        private Texture2D _texture;
+
+        // Relative to screen position
+        private Vector2 _position;
+
+        private Vector2 _drawPosition { get { return _viewManager.camera.ScreenToWorld(_position); } }
+        public RectangleF clickRectangle { get { return new RectangleF(_drawPosition.X, _drawPosition.Y, _texture.Width, _texture.Height); } }
+
+        public ScreenButton(DebugManager debugManager, ViewManager viewManager, Texture2D texture, Vector2 position, ClickManager clickManager)
+        {
+            _debugManager = debugManager;
+            _viewManager = viewManager;
+            _texture = texture;
+            _position = position;
+
+            clickManager.AddScreenLevelLeftClick(this, LeftClickAction);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, _drawPosition, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+        }
+
+        public void LeftClickAction()
+        {
+            _debugManager.AddDebugMessage("Button click action", DebugLevel.Debug);
+        }
+    }
+}
